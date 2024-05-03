7fcd6db47741d090d24c28437fc58e0fa4117c21
Charlotte000 <ramil.mifteev@yandex.ru>
2024-05-03 23:51:26 +0300

    add crack position ui

--- Solution/main2d.py
+++ Solution/main2d.py
@@ -8,7 +8,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.animation import FuncAnimation
 
 
-def animate_wave(a, b, dx, c):
+def animate_wave(a, b, dx, c, crack_positions):
     dt = .707 * dx / c
 
     x_axis = np.arange(0, a * (1 + dx), dx)
@@ -24,7 +24,7 @@ def animate_wave(a, b, dx, c):
     f[:, :, 0] = gaussian_wave(x, y, (0, .5), (.05, .05))
 
     wave_speeds = []
-    crack_positions = [[np.random.rand() * a, np.random.rand() * b] for _ in range(100)]
+    # crack_positions = [[np.random.rand() * a, np.random.rand() * b] for _ in range(100)]
 
     fig, (ax1, ax2) = plt.subplots(2, 1)
 
@@ -77,8 +77,7 @@ def run_simulation():
     b_val = float(b_entry.get())
     dx_val = float(dx_entry.get())
     c_val = float(c_entry.get())
-
-    animate_wave(a_val, b_val, dx_val, c_val)
+    animate_wave(a_val, b_val, dx_val, c_val, crack_positions)
 
 
 
@@ -89,11 +88,13 @@ tk.Label(root, text="Width of the system:").grid(row=0, column=0)
 a_entry = tk.Entry(root)
 a_entry.grid(row=0, column=1)
 a_entry.insert(0, "10.0")
+a_entry.bind('<KeyRelease>', lambda _: canvas.config(width=float(a_entry.get() or '1') * CANVAS_SALE))
 
 tk.Label(root, text="Height of the system (b):").grid(row=1, column=0)
 b_entry = tk.Entry(root)
 b_entry.grid(row=1, column=1)
 b_entry.insert(0, "1.0")
+b_entry.bind('<KeyRelease>', lambda _: canvas.config(height=float(b_entry.get() or '1') * CANVAS_SALE))
 
 tk.Label(root, text="Discrete spatial stepsize (dx):").grid(row=2, column=0)
 dx_entry = tk.Entry(root)
@@ -107,5 +108,28 @@ c_entry.insert(0, "1.0")
 
 run_button = tk.Button(root, text="Run Simulation", command=run_simulation)
 run_button.grid(row=4, columnspan=2)
+
+
+crack_positions = []
+def create_crack(event):
+    crack_positions.append((event.x / CANVAS_SALE, event.y / CANVAS_SALE))
+    canvas.create_rectangle(event.x - CIRCLE_RADIUS, event.y - CIRCLE_RADIUS, event.x + CIRCLE_RADIUS, event.y + CIRCLE_RADIUS)
+
+def create_random_crack(count):
+    for _ in range(count):
+        x, y = (np.random.rand() * float(a_entry.get()), np.random.rand() * float(b_entry.get()))
+        crack_positions.append((x, y))
+        canvas.create_oval(x * CANVAS_SALE - CIRCLE_RADIUS, y * CANVAS_SALE - CIRCLE_RADIUS, x * CANVAS_SALE + CIRCLE_RADIUS, y * CANVAS_SALE + CIRCLE_RADIUS)
+
+
+tk.Button(root, text='Create 100 cracks', command=lambda: create_random_crack(100)).grid(row=5, columnspan=2)
+
+
+CANVAS_SALE = 50
+CIRCLE_RADIUS = 1
+
+canvas = tk.Canvas(root, bg='white', width=10 * CANVAS_SALE, height=1 * CANVAS_SALE)
+canvas.grid(row=6, columnspan=2)
+canvas.bind('<Button-1>', create_crack)
 
 root.mainloop()
